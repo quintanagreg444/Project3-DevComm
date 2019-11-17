66bb604d0c92d0bda6a398ccf0408efeec7632fc
Greg Quintana <quintana.greg444@gmail.com>
2019-11-17 00:19:41 -0700

    added validation and created psot route for user.

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const connectDB = require('./config/db');
 const app = express();
 
 connectDB(); 
+
+// Init Middleware 
+app.use(express.json({extended: false}));
 app.get('/', (req, res) => res.send('API Running'));
 
 //Define Routes
